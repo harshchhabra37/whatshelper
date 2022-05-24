aa13afac1e1b58ab1aad556471d6e2f977b6cc90
hailer24 <anmolbhutanianmol@gmail.com>
2022-05-25 02:38:52 +0530

    resolved lint errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -22,60 +21,64 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   TextEditingController phnController = TextEditingController();
   String initialCountry = "IN";
-  PhoneNumber number =  PhoneNumber(isoCode: 'IN');
+  PhoneNumber number = PhoneNumber(isoCode: 'IN');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-        title: Text('WhatsHelper'),
+        title: const Text('WhatsHelper'),
       ),
       body: Column(
         children: [
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(8)
+                      borderRadius: BorderRadius.circular(8),
                     ),
                     child: Padding(
-                      padding: const EdgeInsets.only(right: 8, bottom: 8, left: 16, top: 8),
+                      padding: const EdgeInsets.only(
+                        right: 8,
+                        bottom: 8,
+                        left: 16,
+                        top: 8,
+                      ),
                       child: InternationalPhoneNumberInput(
-                        searchBoxDecoration: InputDecoration(
-                          icon: Icon(Icons.map)
-                        ),
+                        searchBoxDecoration:
+                            const InputDecoration(icon: Icon(Icons.map)),
                         spaceBetweenSelectorAndTextField: 0,
-                        onInputChanged: (val){
-                            number = val;
+                        onInputChanged: (val) {
+                          number = val;
                         },
-                        selectorConfig: SelectorConfig(
+                        selectorConfig: const SelectorConfig(
                           selectorType: PhoneInputSelectorType.DIALOG,
                           useEmoji: true,
                         ),
                         initialValue: number,
                         textFieldController: phnController,
-                        inputBorder: OutlineInputBorder(),
+                        inputBorder: const OutlineInputBorder(),
                       ),
                     ),
                   ),
                 ),
-                ElevatedButton(onPressed: ()=>whatsAppOpen(number), child: Text('Go to WhatsApp'))
+                ElevatedButton(
+                  onPressed: () => whatsAppOpen(number),
+                  child: const Text('Go to WhatsApp'),
+                )
               ],
             ),
           ),
@@ -83,9 +86,14 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-  void whatsAppOpen(PhoneNumber number) async {
 
-      await launch("whatsapp://send?phone=${number.phoneNumber}");
-
+  Future<void> whatsAppOpen(PhoneNumber number) async {
+    final sendUri = Uri(
+      scheme: 'whatsapp',
+      host: 'send',
+      queryParameters: {'phone': number.phoneNumber},
+    );
+    // "whatsapp://send?phone=${number.phoneNumber}"
+    await launchUrl(sendUri);
   }
 }
